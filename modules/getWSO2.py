4a770d2cbe915bea01914bb426f97b1e70607d90
Rogério Lopes <rogeriolopes.udi@gmail.com>
2021-06-01 16:01:39 -0300

    inclusão de autenticação

--- modules/getWSO2.py
+++ modules/getWSO2.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+from settings.credentials import *
 
 class WSO2():
     def __init__(self):
@@ -8,7 +8,7 @@ class WSO2():
 
     def wso2(self, sysdateWSO2):
         #----------------- fazendo pesquisa no WSO2 para buscar admitidos do dia -----------------
-        newHeaders = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        newHeaders = {'Content-type': 'application/json', 'Accept': 'text/plain', 'AuthUser': CRD_USER_WSO2, 'AuthPassword': CRD_PWD_WSO2}
         response = requests.post('https://wso2-am-gw.algarnet.com.br/employees/transferences',
                                  data="{'date': '"+sysdateWSO2+"'}",
                                  headers=newHeaders,
